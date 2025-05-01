496f9362ad5d696fa77c882b9e20a617ef699462
ALFONSO CIFUENTES ALONSO <76941618+AlfonsoCifuentes@users.noreply.github.com>
2025-05-01 18:59:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,35 +21,6 @@ st.write("Hello, world!")
 #MARKDOWN
 st.markdown("Hello, **world!**")
 
-#LATEX
-st.latex(r'''a^2 + b^2 = c^2''')
-
-#code
-st.code('print("Hello, world!")', language='python')
-
-#iNFORMACIÓN, ADVERTENCIAS Y ERRORES
-st.info("This is an info message")
-st.warning("This is a warning message")
-st.error("This is an error message")
-st.success("This is a success message")
-st.exception("This is an exception message")
-st.help("This is a help message")
-st.json({"key": "value"})
-st.dataframe({"key": "value"})
-st.table({"key": "value"})
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-st.progress(50)
-st.spinner("Loading...")
-st.balloons()
-st.snow()
-st.map()
-st.image("https://via.placeholder.com/150", caption="Placeholder image")
-st.video("https://youtu.be/AOqY6UdJFvo?si=sM1H9E_-6aN-W311")
-st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
-st.file_uploader("Upload a file", type=["csv", "txt"])
-st.download_button("Download a file", data="Hello, world!", file_name="hello.txt")
-
-
 #Importando la libreria pandas para leer el csv
 df = pd.read_csv('titanic_limpio.csv')
 
@@ -68,9 +39,30 @@ tab1, tab2,tab3, tab4 = st.tabs(["Inicio", "Datos", "Gráficos"])
 with tab1:
     st.title("Inicio")
     st.write("Bienvenido a la app de streamlit")
+    #LATEX
+    st.latex(r'''a^2 + b^2 = c^2''')
+
+    #code
+    st.code('print("Hello, world!")', language='python')
+
+    #iNFORMACIÓN, ADVERTENCIAS Y ERRORES
+    st.info("This is an info message")
+    st.warning("This is a warning message")
+    st.error("This is an error message")
+    st.success("This is a success message")
+    st.exception("This is an exception message")
+    st.help("This is a help message")
+    st.json({"key": "value"})
+    st.dataframe({"key": "value"})
+    st.table({"key": "value"})
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    st.progress(50)
+    st.spinner("Loading...")
+    st.map()
     st.image("https://via.placeholder.com/150", caption="Placeholder image")
-    st.video("https://youtu.be/AOqY6UdJFvo?si=sM1H9E_-6aN-W311")
     st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+    st.file_uploader("Upload a file", type=["csv", "txt"])
+    st.download_button("Download a file", data="Hello, world!", file_name="hello.txt")
 
 with tab2:
     st.title("Datos")
